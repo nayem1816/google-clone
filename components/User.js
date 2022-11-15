c9dbc615dceee478f936b0ebeca39fe02d767414
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2022-11-15 22:59:47 +0600

    google sign in added using next auth

--- components/User.js
+++ components/User.js
@@ -1,10 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const User = () => {
+    const { data: session } = useSession();
+    if (session) {
+        return (
+            <>
+                <img
+                    className="h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1"
+                    onClick={signOut}
+                    src={session.user.image}
+                    alt=""
+                />
+            </>
+        );
+    }
     return (
-        <div>
-            <p>user</p>
-        </div>
+        <>
+            <button
+                className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md"
+                onClick={signIn}
+            >
+                Sign in
+            </button>
+        </>
     );
 };
 
