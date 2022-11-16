c0dd10db1a41e1b21b28ddc642867af11ec1a919
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2022-11-16 19:51:21 +0600

    show result in searh page

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Parser from 'html-react-parser';
 
 const SearchResults = ({ results }) => {
     return (
@@ -7,6 +8,26 @@ const SearchResults = ({ results }) => {
                 About {results.searchInformation.formattedTotalResults} results
                 ({results.searchInformation.formattedSearchTime} seconds)
             </p>
+            {results.items.map((result) => (
+                <div key={result.link} className="max-w-xl mb-8">
+                    <div className="group">
+                        <a href={result.link} className="text-sm truncate">
+                            {result.formattedUrl}
+                        </a>
+                        <a
+                            href={result.link}
+                            className="group-hover:underline decoration-blue-800"
+                        >
+                            <h2 className="truncate text-xl font-medium text-blue-800">
+                                {result.title}
+                            </h2>
+                        </a>
+                    </div>
+                    <p className="text-gray-600 text-sm">
+                        {Parser(result.htmlSnippet)}
+                    </p>
+                </div>
+            ))}
         </div>
     );
 };
